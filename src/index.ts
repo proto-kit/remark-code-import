a546997e26ea2a72a7df29bacc55eb9838433622
Raphael Panic <raphael.panic@gmx.at>
2026-01-17 23:17:04 +0100

    Added file import by inline transcript

--- src/index.ts
+++ src/index.ts
@@ -14,26 +14,110 @@ interface CodeImportOptions {
   allowImportingFromOutside?: boolean;
 }
 
+type Element = {
+  type: "text";
+  text: string;
+} | {
+  type: "group";
+  group: string
+}
+
+function getGroup(
+  input: string
+) {
+  const result = /group (?<group>[\w\-_]*)/.exec(input)
+  return result?.groups?.group
+}
+
+function extractElements(
+  input: string
+): Element[] {
+  return input.split(EOL).map<Element>(line => {
+    if(line.includes("group")) {
+      const group = getGroup(line);
+      if(group === undefined) {
+        return {
+          type: "text",
+          text: line
+        }
+      } else {
+        return {
+          type: "group",
+          group
+        }
+      }
+    } else {
+      return {
+        type: "text",
+        text: line
+      }
+    }
+  })
+}
+
+function extractGroups(
+  content: string[]
+) {
+  const map: Record<string, number[]> = {};
+  content.forEach(((line, index) => {
+    if(line.includes("group")) {
+      const group = getGroup(line);
+      if(group !== undefined) {
+        (map[group] ??= []).push(index);
+      }
+    }
+  }));
+  return map;
+}
+
+function transformDoc(
+  content: string[],
+  elements: Element[],
+) {
+  const groups = extractGroups(content);
+  return elements.flatMap(el => {
+    if(el.type === "text") {
+      return [el.text];
+    } else {
+      const array = groups[el.group];
+      if(array && array.length < 2) {
+        console.error(`Group ${el.group} not found in document`)
+      }
+      const group = content.slice(array.shift()! + 1, array.shift());
+      // Not sure if that is necessary
+      groups[el.group] = array;
+      return group;
+    }
+  }).join("\n");
+}
+
 function extractLines(
+  mode: "classic" | "inline",
   content: string,
   fromLine: number | undefined,
   hasDash: boolean,
   toLine: number | undefined,
+  oldValue: string,
   preserveTrailingNewline: boolean = false
 ) {
   const lines = content.split(EOL);
-  const start = fromLine || 1;
-  let end;
-  if (!hasDash) {
-    end = start;
-  } else if (toLine) {
-    end = toLine;
-  } else if (lines[lines.length - 1] === '' && !preserveTrailingNewline) {
-    end = lines.length - 1;
+  if(mode === "classic") {
+    const start = fromLine || 1;
+    let end;
+    if (!hasDash) {
+      end = start;
+    } else if (toLine) {
+      end = toLine;
+    } else if (lines[lines.length - 1] === '' && !preserveTrailingNewline) {
+      end = lines.length - 1;
+    } else {
+      end = lines.length;
+    }
+    return lines.slice(start - 1, end).join('\n');
   } else {
-    end = lines.length;
+    const elements = extractElements(oldValue);
+    return transformDoc(lines, elements);
   }
-  return lines.slice(start - 1, end).join('\n');
 }
 
 function codeImport(options: CodeImportOptions = {}) {
@@ -83,6 +167,8 @@ function codeImport(options: CodeImportOptions = {}) {
         .replace(/\\ /g, ' ');
       const fileAbsPath = path.resolve(file.dirname, normalizedFilePath);
 
+      const mode = (node.meta || '').includes("inline") ? "inline" : "classic";
+
       if (!options.allowImportingFromOutside) {
         const relativePathFromRootDir = path.relative(rootDir, fileAbsPath);
         if (
@@ -106,10 +192,12 @@ function codeImport(options: CodeImportOptions = {}) {
               }
 
               node.value = extractLines(
+                mode,
                 fileContent,
                 fromLine,
                 hasDash,
                 toLine,
+                node.value,
                 options.preserveTrailingNewline
               );
               if (options.removeRedundantIndentations) {
@@ -123,10 +211,12 @@ function codeImport(options: CodeImportOptions = {}) {
         const fileContent = fs.readFileSync(fileAbsPath, 'utf8');
 
         node.value = extractLines(
+          mode,
           fileContent,
           fromLine,
           hasDash,
           toLine,
+          node.value,
           options.preserveTrailingNewline
         );
         if (options.removeRedundantIndentations) {
